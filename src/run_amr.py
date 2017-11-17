846d1fb38e43283cadcb45217723f1010be01de2
gabe cemaj <gabriel@cemaj.com>
2017-11-17 17:37:19 -0500

    ready for test

--- src/run_amr.py
+++ src/run_amr.py
@@ -1,7 +1,7 @@
 from graphcnn.experiment import *
 import pickle
 
-root = "..\\datasets\\amr\\"
+root = ".\\datasets\\amr\\"
 
 adj = np.load(root + 'adjacency.npy')  
 vertex = np.load(root + 'vertex.npy')  
