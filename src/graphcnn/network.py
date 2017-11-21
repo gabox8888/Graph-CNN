330b3a4fc7e0bb33fddfa3ad664474624638cb26
gabe cemaj <gabriel@cemaj.com>
2017-11-21 17:56:27 -0500

    beam search

--- src/graphcnn/network.py
+++ src/graphcnn/network.py
@@ -138,12 +138,12 @@ class GraphCNNNetwork(object):
             def testing_decoder(): 
                 padded_size = 300
                 start_tokens = tf.tile(tf.constant([1], dtype=tf.int32), [batch_size], name='start_tokens')
-                test = tf.contrib.rnn.LSTMStateTuple(tf.contrib.seq2seq.tile_batch(self.current_V, multiplier=10),tf.contrib.seq2seq.tile_batch(self.current_V, multiplier=10)),
+                test = tf.contrib.rnn.LSTMStateTuple(tf.contrib.seq2seq.tile_batch(test[0], multiplier=10),tf.contrib.seq2seq.tile_batch(test[1], multiplier=10)),
                 # helper = tf.contrib.seq2seq.GreedyEmbeddingHelper(dec_embeddings,start_tokens,2)
                 # decoder = tf.contrib.seq2seq.BasicDecoder(cell=dec_cell,helper=helper,initial_state=test,output_layer=output_layer) 
                 decoder = tf.contrib.seq2seq.BeamSearchDecoder(cell=dec_cell,embedding=dec_embeddings,start_tokens=start_tokens,end_token=2,initial_state=test,beam_width=10,output_layer=output_layer,length_penalty_weight=0.0)
                 decder_output = tf.contrib.seq2seq.dynamic_decode(decoder,impute_finished=False,maximum_iterations=padded_size)[0] 
-                self.pred = tf.identity(decder_output.predicted_ids, name='predictions')
+                self.pred = tf.identity(decder_output.sample_id, name='predictions')
                 return training_decoder()
 
 
