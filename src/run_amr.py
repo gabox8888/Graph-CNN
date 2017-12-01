26a1ead6fd5b98990b804cb3012a4b22d004b58b
gabe cemaj <gabriel@cemaj.com>
2017-12-01 16:57:46 -0500

    fixed dataset

--- src/run_amr.py
+++ src/run_amr.py
@@ -3,24 +3,24 @@ import pickle
 
 root = "./datasets/amr/"
 
-# adj = []
-# vertex = []
-# labels = []
-# masks = []
+adj = []
+vertex = []
+labels = []
+masks = []
 
-# for i in range(10312,30000):
-#     if i % 1000 == 0 : print("currently at {}".format(i))
-#     adj += [np.load(root + 'adjacency_{}.npy'.format(i))]
-#     vertex += [np.load(root + 'vertex_{}.npy'.format(i))]  
-#     labels += [np.load(root + 'labels_{}.npy'.format(i))] 
-#     masks += [np.load(root + 'masks_{}.npy'.format(i))]  
+for i in range(10312,100000):
+    if i % 1000 == 0 : print("currently at {}".format(i))
+    adj += [np.load(root + 'adjacency_{}.npy'.format(i))]
+    vertex += [np.load(root + 'vertex_{}.npy'.format(i))]  
+    labels += [np.load(root + 'labels_{}.npy'.format(i))] 
+    masks += [np.load(root + 'masks_{}.npy'.format(i))]  
 
-# adj = np.array(adj)
-# vertex = np.array(vertex)
-# labels = np.array(labels)
-# masks = np.array(masks)
+adj = np.array(adj)
+vertex = np.array(vertex)
+labels = np.array(labels)
+masks = np.array(masks)
 i_word = pickle.load(open(root + 'amr_i_word.pkl','rb'))
-# dataset = [adj,vertex,labels,masks,i_word]
+dataset = [adj,vertex,labels,masks,i_word]
 
 # Decay value for BatchNorm layers, seems to work better with 0.3
 GraphCNNGlobal.BN_DECAY = 0.3
