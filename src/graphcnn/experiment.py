6361e1a5c60009bf8b324846f62f9e4bbe26d598
gabe cemaj <gabriel@cemaj.com>
2017-12-05 22:18:12 -0500

    trying out embedding

--- src/graphcnn/experiment.py
+++ src/graphcnn/experiment.py
@@ -545,6 +545,7 @@ class GraphCNNWithRNNExperiment(GraphCNNExperiment):
         
         largest_graph = max(graph_size)
         print(largest_graph)
+        print(meta[0][:10],"poopy")
 
         def readAndPadNumpy(x):
             temp = np.load(x)
