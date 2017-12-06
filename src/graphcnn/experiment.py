121739c44bfe7502587169b84e9b9281513feaf9
gabe cemaj <gabriel@cemaj.com>
2017-12-05 23:12:11 -0500

    trying out embedding

--- src/graphcnn/experiment.py
+++ src/graphcnn/experiment.py
@@ -541,7 +541,7 @@ class GraphCNNWithRNNExperiment(GraphCNNExperiment):
     def create_data(self):
         meta = pickle.load(open(self.root_dir + "graph_shapes.pkl",'rb'))
 
-        graph_size = np.array([s[0] for s in meta[1]]).astype(np.int64)
+        graph_size = np.array([s[0] for s in meta[1]]).astype(np.float32)
         
         largest_graph = max(graph_size)
         print(largest_graph)
